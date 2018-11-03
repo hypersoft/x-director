d46269b4884a37bd8baf4771f7678c882c023ed7
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-03 12:29:55 -0500

    TODONE

--- sources/bash/source/environment.d/separators.sh
+++ sources/bash/source/environment.d/separators.sh
@@ -1,4 +1,7 @@
-XD_SEP_ROW=$'\n'
-XD_SEP_COL=$' '
+RSI=${RSI:-$'\n'} # row-separator-in
+RSO=${RSO:-$RSI}  # row-separator-out
+CSI=${CSI:-$' \t\n'}  # col-separator-in
+CSO=${CSO:-$CSI}  # col-separator-out
+
 XD_SEP_ERE=$'\1'
 XD_SEP_FILE=:
---
+++ sources/bash/source/functions.d/table.sh
@@ -0,0 +1,32 @@
+function columns() {
+	local -i final_point="${@:$#}"
+	let final_point--;
+	local IFS=$CSI;
+	set -- "${@:1:$# - 1}"
+	local -a columns
+	while read -d "$RSI" -a columns; do
+		for point; do
+			let point--
+			(( point > ${#columns[@]} )) && return 1;
+			printf "%s${CSO}" "${columns[point]}";
+		done;
+		(( final_point > ${#columns[@]} )) && return 1;
+		printf "%s${RSO}" "${columns[final_point]}";
+	done;
+}
+
+function rows() {
+	local IFS="$RSI";
+	local -a rows=("$@");
+	local -i r=0 m=0;
+	for x; do
+		(( x > m )) && m=$x;
+	done;
+	local -a lines;
+	readarray -O 1 -d "$RSI" -n $m lines;
+	local -i x
+	for x; do
+		(( x > ${#lines[@]} )) && return 1;
+		printf "%s" "${lines[$x]/$RSI/$RSO}"
+	done;
+}
