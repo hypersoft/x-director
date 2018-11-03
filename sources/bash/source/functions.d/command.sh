44990e7056d5d61484f09061fede00d0fd747bd4
Triston J. Taylor <pc.wiz.tt@gmail.com>
2018-11-03 03:28:20 -0500

    Improved Command Handling

--- sources/bash/source/functions.d/command.sh
+++ sources/bash/source/functions.d/command.sh
@@ -1,8 +1,5 @@
 
 # allows the user to run a system-command as a function-target
 function command() {
-	key-within-list $1 -- $OPTIONS $FUNCTIONS && {
-		echo "that's an error bob"
-	}
-	"$@";
+	builtin command "$@";
 }
